c6918f60cbd795b2f2635459eacb0d19bb58f2fe
mjeffw <geek.macbeer@gmail.com>
2022-06-08 18:47:18 -0400

    tweaked layout

--- src/module/actor/character/character-sheet.ts
+++ src/module/actor/character/character-sheet.ts
@@ -10,6 +10,12 @@ export class GurpsCharacterSheet extends ActorSheet<ActorSheet.Options, GurpsCha
     return `systems/new-system/module/actor/${this.actor.data.type}/${this.actor.data.type}-sheet.hbs`
   }
 
+  static override get defaultOptions(): ActorSheet.Options {
+    return mergeObject(super.defaultOptions, {
+      width: 650,
+    })
+  }
+
   override getData(options?: Partial<ActorSheet.Options>): GurpsCharacterSheetData | Promise<GurpsCharacterSheetData> {
     const data = super.getData(options) as GurpsCharacterSheetData
     data.config = window.gurps2
@@ -19,11 +25,26 @@ export class GurpsCharacterSheet extends ActorSheet<ActorSheet.Options, GurpsCha
   override activateListeners(html: JQuery<HTMLElement>): void {
     html.find('.accordion').on('click', function (ev) {
       ev.preventDefault()
-      const element = ev.currentTarget
-      $(element).toggleClass('active')
-      const content = $(element).next('.accordion-content')
-      if (content.css('max-height') == '0px') content.css('max-height', '100px')
-      else content.css('max-height', '0px')
+      const element = $(ev.currentTarget)
+      element.toggleClass('active')
+
+      const content = element.next('.accordion-content')
+      toggleBetweenStyles(content, 'max-height', '0px', '100%')
     })
   }
+}
+
+/**
+ * JQuery HMTLElement method.
+ *
+ * If the specified style is currently set to 'choice1', set it to 'choice2'; otherwise set it to 'choice1'.
+ *
+ * @param element: JQuery<HTMLElement> to operate on.
+ * @param style: the name of the style to modify
+ * @param choice1: value1
+ * @param choice2: value2
+ */
+function toggleBetweenStyles(element: JQuery<HTMLElement>, style: string, choice1: string, choice2: string) {
+  if (element.css(style) == choice1) element.css(style, choice2)
+  else element.css(style, choice1)
 }
